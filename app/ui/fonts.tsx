d95df320ca134a8254fd07e07a9410c733fec730
Chandan <38459962+lazycodie@users.noreply.github.com>
2024-09-03 10:40:30 +0530

    Commit all the files into repository
    Commit all the files into repository

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,7 @@
+import { Inter,Lusitana  } from 'next/font/google'; 
+export const inter = Inter({ subsets: ['latin'] });
+// Importing the Lusitana font
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
